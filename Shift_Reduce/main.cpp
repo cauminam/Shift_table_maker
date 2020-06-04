0ab453f43fd87890cf0b873dc14483fc040d05c9
Cauminam <uigohiiming2@naver.com>
2020-06-05 05:15:14 +0900

    Initial Commit

---
+++ Shift_Reduce/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Shift_Reduce
+//
+//  Created by 양준식 on 2020/06/05.
+//  Copyright © 2020 양준식. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
